df5a3eeb6b3ad8e1166695506bea817fff521a6e
Pravin Salgaonkar <pravinsalg@gmail.com>
2025-06-23 21:37:08 +0530

    updated nvim config on 23 June

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 local opt = vim.opt
+local global = vim.g
+
 opt.wrap = true
-vim.g.autoformat = false
-vim.g.lazyvim_eslint_auto_format = true
+
+global.autoformat = false
