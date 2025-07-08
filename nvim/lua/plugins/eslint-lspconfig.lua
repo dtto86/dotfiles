2dd7a4a76f5bf73f8020ec3739927cbbcdc5a060
Pravin Salgaonkar <pravin.salgaonkar@oneadvanced.com>
2025-07-08 22:16:05 +0530

    nvim config

--- nvim/lua/plugins/eslint-lspconfig.lua
+++ nvim/lua/plugins/eslint-lspconfig.lua
@@ -7,27 +7,32 @@ return {
     servers = {
       eslint = {
         settings = {
-          eslint = {
+          -- eslint = {
             -- Dynamically set `nodePath` per file
-            nodePath = (function()
-              local Path = require("plenary.path")
-              -- local util = require("lspconfig.util")
+          nodePath = (function()
+            local Path = require("plenary.path")
+            -- local util = require("lspconfig.util")
 
-              local fname = vim.api.nvim_buf_get_name(0)
-              local dir = vim.fn.fnamemodify(fname, ":p:h")
-              local current = Path:new(dir)
+            local fname = vim.api.nvim_buf_get_name(0)
+            local dir = vim.fn.fnamemodify(fname, ":p:h")
+            local current = Path:new(dir)
 
-              while current.filename ~= current:parent().filename do
-                if current:joinpath("node_modules"):is_dir() then
-                  return current:absolute()
-                end
-                current = current:parent()
+            while current.filename ~= current:parent().filename do
+              if current:joinpath("node_modules"):is_dir() then
+                print("Found node_modules in: " .. current:absolute())
+                return current:absolute()
               end
+              current = current:parent()
+            end
 
-              return vim.fn.getcwd() -- fallback to CWD
-            end)(),
-            workingDirectory = { mode = "auto" },
-          },
+            print("No node_modules found, using default path")
+            -- return vim.fn.getcwd() -- fallback to CWD
+            -- return "/home/pravin/.nvm/versions/node/v20.18.0/lib/node_modules/eslint/"
+            return "/home/pravin/.nvm/versions/node/v20.18.0/lib/node_modules"
+            -- return "/home/pravin/.nvm/versions/node/v20.18.0/bin"
+          end)(),
+          workingDirectories = { mode = "auto" },
+          -- },
           format = { enable = false }, -- formatting handled separately
         },
         root_dir = function(fname)
---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "LazyFile",
+  opts = {
+    git = {
+      diff_opts = {
+        ignore_space_at_eol = true,
+      }
+    }
+  }
+}
